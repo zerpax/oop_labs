6a71191b797de446e92110675a757ac464a5575e
Zerpax <nik_fedorov_2004@mail.ru>
2023-09-29 18:07:31 +0300

    added full test coverage

--- test/test.cpp
+++ test/test.cpp
@@ -16,6 +16,31 @@ TEST(basic_test_set, test_3)
     ASSERT_TRUE(sad("tuesday",222)==true);
 }
 
+TEST(basic_test_set, test_4)
+{
+    ASSERT_TRUE(sad("wednesday",34)==true);
+}
+
+TEST(basic_test_set, test_5)
+{
+    ASSERT_TRUE(sad("thursday",0)==true);
+}
+
+TEST(basic_test_set, test_6)
+{
+    ASSERT_TRUE(sad("friday",100)==true);
+}
+
+TEST(basic_test_set, test_7)
+{
+    ASSERT_TRUE(sad("saturday",56)==true);
+}
+
+TEST(basic_test_set, test_8)
+{
+    ASSERT_TRUE(sad("sunday",-666)==true);
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
